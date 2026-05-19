19781587269db0a23020532541ce9dc7b4fe3799
codusTatevikus <tatev.srapivonyan@gmail.com>
2026-05-19 19:42:50 +0400

    HrSQL

---
+++ SQL/Weather_Observation_Station_8.sql
@@ -0,0 +1,7 @@
+SELECT city
+FROM station
+WHERE
+    (city LIKE 'A%' OR city LIKE 'E%' OR city LIKE 'I%' OR city LIKE 'O%' OR city LIKE 'U%')
+AND
+    (city LIKE '%a' OR city LIKE '%e' OR city LIKE '%i' OR city LIKE '%o' OR city LIKE '%u')
+GROUP BY city;
